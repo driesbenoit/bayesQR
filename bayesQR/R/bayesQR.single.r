f7a7f30e587410a42aad93c83a4dde36ff1687c7
Dries F. Benoit <dries.benoit@ugent.be>
2015-09-23 13:03:33 +0200

    update bayesQR.single for both QRb as QRc

--- bayesQR/R/bayesQR.single.r
+++ bayesQR/R/bayesQR.single.r
@@ -165,76 +165,84 @@ bayesQR.single <- function(formula=NULL, data=NULL, quantile=0.5, alasso=FALSE, 
 	# QRb
 	#================================
 	} else if (QRb) {
-	  # If missing, set default prior 
-    if (is.null(prior)) {
-		  prior <- prior(formula=formula, data=data, alasso=alasso)
+		# If missing, set default prior 
+		if (is.null(prior)) {
+			prior <- prior(formula=formula, data=data, alasso=alasso)
 		# Else check provided prior
-    } else if (class(prior)!="bayesQR.prior"){
+		} else if (class(prior)!="bayesQR.prior"){
 			pandterm("Incorrect prior: type '?prior' for more information about how to define priors")
-	  } else if (prior$method!="QRb") {
+		} else if (prior$method!="QRb") {
 			pandterm("Incorrect prior: type '?prior' for more information about how to define priors")
-	  }
+		} else if (normal.approx){
+			print("NOTE: normal approximation of the posterior is not supported for binary dependent variables")
+		}
 
-    # Assign correct variable types
-    n <- as.integer(n)
-    nvar <- as.integer(nvar)
-    ndraw <- as.integer(ndraw)
-    keep <- as.integer(keep)
-    y <- as.integer(y)
-    quantile <- as.double(quantile)
-    X <- as.double(X)
-    beta0 <- as.double(prior$beta0)
-    V0i <- as.double(chol2inv(chol(prior$V0)))
-    betadraw <- double(nvar*ndraw/keep)
-    sigmadraw <- double(ndraw/keep)
+		# Assign correct variable types
+		n <- as.integer(n)
+		nvar <- as.integer(nvar)
+		ndraw <- as.integer(ndraw)
+		keep <- as.integer(keep)
+		y <- as.integer(y)
+		quantile <- as.double(quantile)
+		X <- as.double(X)
+		beta0 <- as.double(prior$beta0)
+		V0i <- as.double(chol2inv(chol(prior$V0)))
+		betadraw <- double(nvar*ndraw/keep)
+		sigmadraw <- double(ndraw/keep)
+		
+		# Call Fortran routine
+		fn_val <- .Fortran("QRb_mcmc", n, nvar, ndraw, keep, y, quantile, X, beta0, V0i, betadraw)
 
-    # Call Fortran routine
-    fn_val <- .Fortran("QRb_mcmc", n, nvar, ndraw, keep, y, quantile, X, beta0, V0i, betadraw)
-
-    # Return bayesQR object
+		# Return bayesQR object
 		out <- list(method="QRb",
-		            quantile=quantile, 
-								names=names,
-                betadraw=matrix(fn_val[[10]], nrow=ndraw/keep,ncol=nvar)
-					  		)
-
+			    normal.approx=FALSE,
+			    quantile=quantile, 
+			    names=names,
+			    betadraw=matrix(fn_val[[10]], nrow=ndraw/keep,ncol=nvar),
+			    sigma.normal=rep(FALSE,nvar*nvar)
+			    )
+	
 	# QRb.AL
 	#================================
 	} else if (QRb.AL) {
-	  # If missing, set default prior 
-    if (is.null(prior)) {
-		  prior <- prior(formula=formula, data=data, alasso=alasso)
+		# If missing, set default prior 
+		if (is.null(prior)) {
+			prior <- prior(formula=formula, data=data, alasso=alasso)
 		# Else check provided prior
-    } else if (class(prior)!="bayesQR.prior"){
+		} else if (class(prior)!="bayesQR.prior"){
 			pandterm("Incorrect prior: type '?prior' for more information about how to define priors")
-	  } else if (prior$method!="QRb.AL") {
+		} else if (prior$method!="QRb.AL") {
 			pandterm("Incorrect prior: type '?prior' for more information about how to define priors")
-	  }
-	
-    # Assign correct variable types
-    n <- as.integer(n)
-    nvar <- as.integer(nvar)
-    ndraw <- as.integer(ndraw)
-    keep <- as.integer(keep)
-    y <- as.integer(y)
-    quantile <- as.double(quantile)
-    x <- as.double(X)
-    c <- as.double(prior$c)
-    d <- as.double(prior$d)
-    betadraw <- double(nvar*ndraw/keep)
-
-    # Call Fortran routine
-    fn_val <- .Fortran("QRb_AL_mcmc",n, nvar, ndraw, keep, y, quantile, x, c, d, betadraw) 
+		} else if (normal.approx){
+			print("NOTE: normal approximation of the posterior is not supported for binary dependent variables")
+		}
+		
+		# Assign correct variable types
+		n <- as.integer(n)
+		nvar <- as.integer(nvar)
+		ndraw <- as.integer(ndraw)
+		keep <- as.integer(keep)
+		y <- as.integer(y)
+		quantile <- as.double(quantile)
+		x <- as.double(X)
+		c <- as.double(prior$c)
+		d <- as.double(prior$d)
+		betadraw <- double(nvar*ndraw/keep)
+		
+		# Call Fortran routine
+		fn_val <- .Fortran("QRb_AL_mcmc",n, nvar, ndraw, keep, y, quantile, x, c, d, betadraw) 
 
 		# Return bayesQR object
 		out <- list(method="QRb.AL",
-	              quantile=quantile,
-								names=names,
-	              betadraw=matrix(fn_val[[10]], nrow=ndraw/keep, ncol=nvar)
-	              )
-	
+			    normal.approx=FALSE,
+			    quantile=quantile,
+			    names=names,
+			    betadraw=matrix(fn_val[[10]], nrow=ndraw/keep, ncol=nvar),
+			    sigma.normal=rep(FALSE,nvar*nvar)
+			    )
+		
 	} else {
-	  pandterm("Something is wrong with the dependent variable")
+		pandterm("Something is wrong with the dependent variable")
 	}
-	return(out)
+return(out)
 } 
